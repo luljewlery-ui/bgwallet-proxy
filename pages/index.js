6b515c75d62e3b012f3443b2f29eab63c128d41a
luljewlery-ui <luljewlery@gmail.com>
2025-08-17 11:49:39 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
+      <h1>✅ BG Wallet Proxy is deployed</h1>
+      <p>API endpoint: <code>/api/proxy</code></p>
+    </div>
+  );
+}
