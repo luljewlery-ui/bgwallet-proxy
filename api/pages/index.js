dc144079fa0b37b7d01e9bb9c5cf1d53d09d2735
luljewlery-ui <luljewlery@gmail.com>
2025-08-17 11:48:25 +0100

    Delete api/pages/index.js

--- api/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
-      <h1>✅ BG Wallet Proxy is up</h1>
-      <p>Use <code>/api/proxy</code> for proxy requests.</p>
-    </div>
-  );
-}
