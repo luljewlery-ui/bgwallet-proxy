58f8b4fe4fa4903b4241092f0a1680432988105c
luljewlery-ui <luljewlery@gmail.com>
2025-08-17 12:39:19 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ fontFamily: "Arial", padding: 40 }}>
-      <h1>✅ BG Wallet Proxy is Deployed!</h1>
-      <p>Test API at <a href="/api/proxy">/api/proxy</a></p>
-    </main>
-  );
-}
