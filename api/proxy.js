ea650c4fcbd4c3a106b3a8af93ccfc1f1a38a8c7
luljewlery-ui <luljewlery@gmail.com>
2025-08-17 11:47:59 +0100

    Delete api/proxy.js

--- api/proxy.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    return res.status(200).json({ message: "✅ BG Wallet proxy is up." });
-  }
-  if (req.method === "POST") {
-    return res.status(200).json({ message: "✅ POST received", data: req.body });
-  }
-  return res.status(405).json({ error: "Method not allowed" });
-}
